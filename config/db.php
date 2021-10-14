8f1475a4e6b3350d287e9804b8f34f1fdd4fb75c
Tero Laakso <laakso_tero@yahoo.com>
2021-10-14 08:40:06 +0300

    aamu

--- config/db.php
+++ config/db.php
@@ -8,10 +8,10 @@
         session_start();
     }
 
-    $hostname = "";
-    $username = "";
-    $password = "";
-    $dbname = "";
+    $hostname = "127.0.0.1";
+    $username = "azure";
+    $password = "6#vWHD_$";
+    $dbname = "your_database_name";
     
     $connection = mysqli_connect($hostname, $username, $password, $dbname) or die("Database connection not established.")
 
