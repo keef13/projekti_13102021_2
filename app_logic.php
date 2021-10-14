394b3a06ba723c0bf4b3bcc3f8991ac3a1bb5e05
Tero Laakso <laakso_tero@yahoo.com>
2021-10-14 10:38:43 +0300

    10:38

--- app_logic.php
+++ app_logic.php
@@ -18,19 +18,22 @@ session_start();
 $errors = [];
 $user_id = "";
 // connect to database
-$db = mysqli_connect('localhost', 'root', '', 'your_database_name');
+//$db = mysqli_connect('localhost', 'root', '', 'your_database_name');
+include('./config/db.php');
+
+
 
  echo $_POST['login_user'];
 
 // LOG USER IN
 if (isset($_POST['login_user'])) {
   // Get username and password from login form
-  $user_id = mysqli_real_escape_string($db, $_POST['user_id']);
+  $user_id = mysqli_real_escape_string($connection, $_POST['user_id']);
   
   
  
   
-  $password = mysqli_real_escape_string($db, $_POST['password']);
+  $password = mysqli_real_escape_string($connection, $_POST['password']);
   // validate form
   if (empty($user_id)) array_push($errors, "Username or Email is required");
   if (empty($password)) array_push($errors, "Password is required");
@@ -39,7 +42,7 @@ if (isset($_POST['login_user'])) {
   if (count($errors) == 0) {
     $password = md5($password);
     $sql = "SELECT * FROM users WHERE username='$user_id' OR email='$user_id' AND password='$password'";
-    $results = mysqli_query($db, $sql);
+    $results = mysqli_query($connection, $sql);
 
     if (mysqli_num_rows($results) == 1) {
       $_SESSION['username'] = $user_id;
@@ -56,10 +59,10 @@ if (isset($_POST['login_user'])) {
   Send email to user to reset their password
 */
 if (isset($_POST['reset-password'])) {
-  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $email = mysqli_real_escape_string($connection, $_POST['email']);
   // ensure that the user exists on our system
   $query = "SELECT email FROM users WHERE email='$email'";
-  $results = mysqli_query($db, $query);
+  $results = mysqli_query($connection, $query);
 
   if (empty($email)) {
     array_push($errors, "Your email is required");
@@ -72,7 +75,7 @@ if (isset($_POST['reset-password'])) {
   if (count($errors) == 0) {
     // store token in the password-reset database table against the user's email
     $sql = "INSERT INTO password_reset(email, token) VALUES ('$email', '$token')";
-    $results = mysqli_query($db, $sql);
+    $results = mysqli_query($connection, $sql);
 	
 	// alkaa 12102021
 	echo "moro";
@@ -125,8 +128,8 @@ if(!$mail->send()){
 
 // ENTER A NEW PASSWORD
 if (isset($_POST['new_password'])) {
-  $new_pass = mysqli_real_escape_string($db, $_POST['new_pass']);
-  $new_pass_c = mysqli_real_escape_string($db, $_POST['new_pass_c']);
+  $new_pass = mysqli_real_escape_string($connection, $_POST['new_pass']);
+  $new_pass_c = mysqli_real_escape_string($connection, $_POST['new_pass_c']);
 
   // Grab to token that came from the email link
   $token = $_SESSION['token'];
@@ -135,13 +138,13 @@ if (isset($_POST['new_password'])) {
   if (count($errors) == 0) {
     // select email address of user from the password_reset table 
     $sql = "SELECT email FROM password_reset WHERE token='$token' LIMIT 1";
-    $results = mysqli_query($db, $sql);
+    $results = mysqli_query($connection, $sql);
     $email = mysqli_fetch_assoc($results)['email'];
 
     if ($email) {
       $new_pass = md5($new_pass);
       $sql = "UPDATE users SET password='$new_pass' WHERE email='$email'";
-      $results = mysqli_query($db, $sql);
+      $results = mysqli_query($connection, $sql);
       header('location: index.php');
     }
   }
