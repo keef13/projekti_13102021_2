8da64a44bd44229dcce2c0822973187d7e51c360
Tero Laakso <laakso_tero@yahoo.com>
2021-10-15 10:03:47 +0300

    dfgdfg

--- rekisteroidy.php
+++ rekisteroidy.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="style.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	
 	<script>
 
@@ -32,7 +32,7 @@ $(function() {
 <header> 
   <div class="content">
     
-    <a class="logo" href="#">Logo</a>
+    <a class="logo" href="#">Blogi</a>
     <div class="toggler"><i class="fa fa-bars fa-2x" aria-hidden="true"></i></div>
     
     <!-- Start Navigation -->
