1149389eb34c9b0235b8603b0c134a105e0b4fab
Tero Laakso <laakso_tero@yahoo.com>
2021-10-15 10:21:39 +0300

    sdfsdssff

--- kirjaudu.php
+++ kirjaudu.php
@@ -11,7 +11,14 @@
 	<link rel="stylesheet" href="https://code.jquery.com/jquery-3.2.1.min.js">
 	
 	<link rel="stylesheet" href="https://use.fontawesome.com/245137f78c.js">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	 <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> 
 	
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
