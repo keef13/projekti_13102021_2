63edef197e10c69c39756e484c2f08346ec7af4b
Tero Laakso <laakso_tero@yahoo.com>
2021-10-15 10:35:42 +0300

    ffghfffffh

--- kirjaudu.php
+++ kirjaudu.php
@@ -15,10 +15,10 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
 <!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
 
 <!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> 
+<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> -->
 	
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -39,11 +39,11 @@ i=i+1;
 
 }
 
-/*$(function() {
+$(function() {
 	$('.toggler').on('click', function() {
 		$('nav').slideToggle(500);
   });
-});*/
+});
 
 
 </script>
