70d0bb3df0e27df26c3dab259ea16fc777c2464e
Tero Laakso <laakso_tero@yahoo.com>
2021-10-20 11:36:27 +0300

    email

--- rekisteroidy/controllers/register.php
+++ rekisteroidy/controllers/register.php
@@ -106,9 +106,7 @@ use PHPMailer\PHPMailer\Exception;
                     if($sqlQuery) {
                      
 					//03102021
-					$msg = 'Click on the activation link to verify your email. <br><br>
-                          <a href="//terol.azurewebsites.net/user_verification.php?token='.$token.'"> Click here to verify email</a>
-                        ';
+					$msg = 'Click on the activation link to verify your email. <br><br><a href="//terol.azurewebsites.net/user_verification.php?token='.$token.'"> Click here to verify email</a>';
 					$emailFrom = "terol@vivaldi.net";
 					$emailFromName = "Blogi";
 					$emailToName = "";
