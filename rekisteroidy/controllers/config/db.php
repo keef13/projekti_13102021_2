4d09c21b055f367a0073b0e22624f04696fe9247
Tero Laakso <laakso_tero@yahoo.com>
2021-10-20 17:21:48 +0300

    17:21

--- rekisteroidy/controllers/config/db.php
+++ rekisteroidy/controllers/config/db.php
@@ -8,9 +8,9 @@
         session_start();
     }
 
-    $hostname = "127.0.0.1:51749";
-    $username = "azure";
-    $password = "6#vWHD_$";
+    $hostname = "localhost";
+    $username = "root";
+    $password = "";
     $dbname = "your_database_name";
     
     $connection = mysqli_connect($hostname, $username, $password, $dbname) or die("Database connection not established.")
