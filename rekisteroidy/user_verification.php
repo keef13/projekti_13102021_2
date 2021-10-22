8924e50a33bafeb24d8aa7804f7ee464b0bb8e19
Tero Laakso <laakso_tero@yahoo.com>
2021-10-22 12:29:19 +0300

    12.29

--- rekisteroidy/user_verification.php
+++ rekisteroidy/user_verification.php
@@ -19,14 +19,14 @@
 
     <div class="container">
         <div class="jumbotron text-center">
-            <h1 class="display-4">User Email Verification Demo</h1>
+            <h1 class="display-4">Käyttäjätunnus on aktivoitu</h1>
             <div class="col-12 mb-5 text-center">
                 <?php echo $email_already_verified; ?>
                 <?php echo $email_verified; ?>
                 <?php echo $activation_error; ?>
             </div>
-            <p class="lead">If user account is verified then click on the following button to login.</p>
-            <a class="btn btn-lg btn-success" href="//terol.azurewebsites.net/kirjaudu.php"
+            <p class="lead">Kirjaudu blogiin klikkaamalla painiketta</p>
+            <a class="btn btn-lg btn-success" href="//terol.azurewebsites.net/blogi.php"
                >Click to Login
             </a>
         </div>
