c23db780e1daed1699cce1817c28b4b8c9680ede
Tero Laakso <laakso_tero@yahoo.com>
2021-10-24 17:39:52 +0300

    24102021

--- rekisteroidy/controllers/register.php
+++ rekisteroidy/controllers/register.php
@@ -111,18 +111,18 @@ use PHPMailer\PHPMailer\Exception;
 					$emailFromName = "Blogi";
 					$emailToName = "";
 					$mail = new PHPMailer;
-						$mail->isSMTP(); 
-						$mail->CharSet = 'UTF-8';
-	$mail->SMTPDebug = 0; // 0 = off (for production use) - 1 = client messages - 2 = client and server messages
-	$mail->Host = "smtp.vivaldi.net"; // use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
-	$mail->Port = 587; // TLS only
-	$mail->SMTPSecure = 'tls'; // ssl is depracated
-	$mail->SMTPAuth = true;
-	$mail->Username = "terol@vivaldi.net";
-	$mail->Password = "Storykeef#13!!!";
-	$mail->setFrom($emailFrom, $emailFromName);
-	$mail->addAddress($email, $emailToName);
-	$mail->Subject = "Aktivoi käyttäjätunnus";
+					$mail->isSMTP(); 
+					$mail->CharSet = 'UTF-8';
+	                $mail->SMTPDebug = 0; // 0 = off (for production use) - 1 = client messages - 2 = client and server messages
+	                $mail->Host = "smtp.vivaldi.net"; // use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
+	                $mail->Port = 587; // TLS only
+	                $mail->SMTPSecure = 'tls'; // ssl is depracated
+	                $mail->SMTPAuth = true;
+	                $mail->Username = "terol@vivaldi.net";
+	                $mail->Password = "Storykeef#13!!!";
+	                $mail->setFrom($emailFrom, $emailFromName);
+	                $mail->addAddress($email, $emailToName);
+	                $mail->Subject = "Aktivoi käyttäjätunnus";
 	
 	
     $msg = wordwrap($msg,70);
